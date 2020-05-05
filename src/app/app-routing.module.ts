820fb99db4de74253608ddba62bcb9934e2327db
Filipe Bastias <filipebastias@hotmail.com>
2020-05-05 23:23:57 +0100

    Added Angular Material. Created Generator Component. Route to generator page working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GeneratorComponent } from './generator/generator.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'generator', component: GeneratorComponent },
+  { path: '**', redirectTo: 'generator' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/generator/generator.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  templateUrl: './generator.component.html',
+  styleUrls: ['./generator.component.scss']
+})
+export class GeneratorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
